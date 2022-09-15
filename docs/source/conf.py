d22dec2bf8ea464946cc91357091ff866d1a7886
triasteran <47274795+triasteran@users.noreply.github.com>
2022-09-15 13:52:19 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RiboGalaxy'
+copyright = '2022, Alla Fedorova'
+author = 'Alla Fedorova'
 
 release = '0.1'
 version = '0.1.0'
